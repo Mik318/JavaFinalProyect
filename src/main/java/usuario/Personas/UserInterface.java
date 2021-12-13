1964751dfee4a8718ba8d307cd41124eb35690a3
Mik318 <lokilskdij@gmail.com>
2021-12-12 19:36:46 -0600

    end proyect with User interface

---
+++ src/main/java/usuario/Personas/UserInterface.java
@@ -0,0 +1,9 @@
+package usuario.Personas;
+public class UserInterface {
+    public static void main(String[] args) {
+    Vista vista = new Vista();
+    vista.setVisible(true);
+    vista.setResizable(false);
+    vista.setLocationRelativeTo(null);
+    }
+}
--- src/main/java/usuario/UserInterface.java
+++
@@ -1,7 +0,0 @@
-package usuario;
-public class UserInterface {
-    public static void main(String[] args) {
-    Vista vista = new Vista();
-    vista.setVisible(true);
-    }
-}
